980503732bfd62a71b0eb7d0b0e778a305b01828
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-04 18:20:21 +0530

    pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import HomeCards from '../components/HomeCards'
+import JobListings from '../components/JobListings'
+import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
   return (
-    <Hero />
+    <> 
+        <Hero />
+        <HomeCards />
+        <JobListings isHome = {true} />
+        <ViewAllJobs />
+    </>
   )
 }
 
