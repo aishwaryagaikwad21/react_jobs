bb02372f452f38868698cbabe8fc30eda707e3a8
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-04 18:24:17 +0530

    browse jobs

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import jobs from '../jobs.json'
 import JobListing from './JobListing'
 
-const JobListings = () => {
+const JobListings = ({isHome = false}) => {
    // console.log(jobs) //array of jobs
 
-   const recentJobListings = jobs.slice(0, 3) //to show only 3 job cards and hide rest
+   const jobListings = isHome ? jobs.slice(0,3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? 'Recent Jobs': 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           
-        { recentJobListings.map((job) => {
+        { jobListings.map((job) => {
           return <JobListing key={job.id} job={job}/>
           } ) }
         </div>
