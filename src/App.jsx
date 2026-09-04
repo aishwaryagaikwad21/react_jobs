39b312e41628066d85e348ee3182ab3fffba7a60
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-09-04 21:04:56 +0530

    fetch single job using data loader from React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
-import SingleJob from './pages/SingleJob'
+import SingleJob, {jobLoader} from './pages/SingleJob'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element= {<JobsPage />} />
-      <Route path='/jobs/:id' element={<SingleJob />} />
+      <Route path='/jobs/:id' element={<SingleJob />} loader={jobLoader}/>
       <Route path='*' element= {<NotFoundPage />} />
     </Route>
   )
